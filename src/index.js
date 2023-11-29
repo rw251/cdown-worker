7798108816b208a2702f3abfe02b38c89ab68eb5
Richard Williams <1234richardwilliams@gmail.com>
2023-11-29 10:04:14 +0000

    Allow to reget episode

--- src/index.js
+++ src/index.js
@@ -53,49 +53,10 @@ async function internalGetEpisode({ episodeNumber }) {
 	return { episode, data };
 }
 
-const getNextEpisode = async (env) => {
-	// Get most recent successful episode date
-	const LAST_SUCCESSFUL_EPISODE_DATE = await env.CDOWN_KV.get('LAST_SUCCESSFUL_EPISODE_DATE');
-	// Calculate next episode date and time to get
-	const NEXT_EPISODE_TO_GET_DATE_AND_TIME = getNextEpisodeDateAndTimeAndTime(new Date(LAST_SUCCESSFUL_EPISODE_DATE));
-
-	const NOW = new Date();
-	const shouldGetNextEpisode = NOW > NEXT_EPISODE_TO_GET_DATE_AND_TIME;
-
-	if (!shouldGetNextEpisode) {
-		// Write log and exit
-		await log(
-			env,
-			`${NOW}: Last success was on ${LAST_SUCCESSFUL_EPISODE_DATE}. Next episode check at ${NEXT_EPISODE_TO_GET_DATE_AND_TIME}. In the future so stopping.`
-		);
-		await writeLog(env);
-		return false;
-	}
-
-	// Get most recent successful episode number
-	const LAST_SUCCESSFUL_EPISODE_NUMBER = await env.CDOWN_KV.get('LAST_SUCCESSFUL_EPISODE_NUMBER');
-
-	const seriesData = await env.CDOWN_BUCKET.get(SERIES_FILE);
-	const series = seriesData ? await seriesData.json() : { Series_88: { a: '2023-06-30', b: '2023-10-20', c: 8051, d: 8131 } };
-
+async function doPlayers(env, episode) {
 	const playersData = await env.CDOWN_BUCKET.get(PLAYERS_FILE);
 	const players = playersData ? await playersData.json() : { Barry_Wood: [8130, 8131] };
 
-	Object.keys(series).forEach((key) => {
-		series[key].a = new Date(series[key].a);
-		series[key].b = new Date(series[key].b);
-	});
-
-	const maxEpisode = Math.max(Object.values(series).map((x) => x.d));
-	// const lastEpisode = await env.CDOWN_BUCKET.get(`${maxEpisode}.json`).then((resp) => resp.json());
-	const episodeNumber = +LAST_SUCCESSFUL_EPISODE_NUMBER + 1;
-	console.log(episodeNumber);
-
-	const { episode, data, nodatayet } = await internalGetEpisode({ episodeNumber });
-
-	if (nodatayet) {
-		return false;
-	}
 	if (!players[episode.p1.l]) {
 		players[episode.p1.l] = [];
 	}
@@ -106,7 +67,18 @@ const getNextEpisode = async (env) => {
 	players[episode.p2.l].push(episode.e);
 	players[episode.p1.l] = [...new Set(players[episode.p1.l])];
 	players[episode.p2.l] = [...new Set(players[episode.p2.l])];
-	// console.log(players);
+
+	return players;
+}
+
+async function doSeries(env, episode) {
+	const seriesData = await env.CDOWN_BUCKET.get(SERIES_FILE);
+	const series = seriesData ? await seriesData.json() : { Series_88: { a: '2023-06-30', b: '2023-10-20', c: 8051, d: 8131 } };
+
+	Object.keys(series).forEach((key) => {
+		series[key].a = new Date(series[key].a);
+		series[key].b = new Date(series[key].b);
+	});
 
 	if (!series[episode.s.l]) {
 		series[episode.s.l] = {
@@ -139,7 +111,41 @@ const getNextEpisode = async (env) => {
 		series[s].a = series[s].a.toISOString().substr(0, 10);
 		series[s].b = series[s].b.toISOString().substr(0, 10);
 	});
-	// console.log(series);
+}
+
+const getNextEpisode = async (env) => {
+	// Get most recent successful episode date
+	const LAST_SUCCESSFUL_EPISODE_DATE = await env.CDOWN_KV.get('LAST_SUCCESSFUL_EPISODE_DATE');
+	// Calculate next episode date and time to get
+	const NEXT_EPISODE_TO_GET_DATE_AND_TIME = getNextEpisodeDateAndTimeAndTime(new Date(LAST_SUCCESSFUL_EPISODE_DATE));
+
+	const NOW = new Date();
+	const shouldGetNextEpisode = NOW > NEXT_EPISODE_TO_GET_DATE_AND_TIME;
+
+	if (!shouldGetNextEpisode) {
+		// Write log and exit
+		await log(
+			env,
+			`${NOW}: Last success was on ${LAST_SUCCESSFUL_EPISODE_DATE}. Next episode check at ${NEXT_EPISODE_TO_GET_DATE_AND_TIME}. In the future so stopping.`
+		);
+		await writeLog(env);
+		return false;
+	}
+
+	// Get most recent successful episode number
+	const LAST_SUCCESSFUL_EPISODE_NUMBER = await env.CDOWN_KV.get('LAST_SUCCESSFUL_EPISODE_NUMBER');
+
+	const episodeNumber = +LAST_SUCCESSFUL_EPISODE_NUMBER + 1;
+	console.log(episodeNumber);
+
+	const { episode, data, nodatayet } = await internalGetEpisode({ episodeNumber });
+
+	if (nodatayet) {
+		return false;
+	}
+
+	const players = await doPlayers(env, episode);
+	const series = await doSeries(env, episode);
 
 	await env.CDOWN_KV.put(episodeNumber, data);
 	await env.CDOWN_BUCKET.put(`${episodeNumber}.json`, JSON.stringify(episode));
@@ -232,6 +238,15 @@ export default {
 		} else if (request.url.indexOf('get') > -1) {
 			const ep = +request.url.split('/get')[1].replace(/[^0-9]/g, '');
 			const { episode, data } = await internalGetEpisode({ episodeNumber: ep });
+
+			const players = await doPlayers(env, episode);
+			const series = await doSeries(env, episode);
+
+			await env.CDOWN_KV.put(ep, data);
+			await env.CDOWN_BUCKET.put(`${ep}.json`, JSON.stringify(episode));
+			await env.CDOWN_BUCKET.put(PLAYERS_FILE, JSON.stringify(players));
+			await env.CDOWN_BUCKET.put(SERIES_FILE, JSON.stringify(series));
+
 			const messages = getMessages();
 			return new Response(JSON.stringify({ messages, episode, data }), {
 				headers: {
